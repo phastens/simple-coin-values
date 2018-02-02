10b0c36ed5cb22f72197409e4f469f4824826f49
Stéphane Bonnemains <stephane.bonnemains@jcdecaux.com>
2018-02-02 07:27:56 +0100

    Add coin without any in bank

--- my_coin_values.py
+++ my_coin_values.py
@@ -31,35 +31,39 @@ def colorT(number):
         return "green"
 
 for currencie in list_curr:
-    url = "https://api.coinmarketcap.com/v1/ticker/" + currencie + "/?convert=EUR"
+    url = 'https://api.coinmarketcap.com/v1/ticker/' + currencie + '/?convert=EUR'
     response_json = urllib2.urlopen(url)
     data = json.loads(response_json.read())
-    value = data[0]["price_usd"]
-    percent_h = data[0]["percent_change_1h"]
-    percent_d = data[0]["percent_change_24h"]
-    percent_w = data[0]["percent_change_7d"]
+    value = data[0]['price_usd']
+    percent_h = data[0]['percent_change_1h']
+    percent_d = data[0]['percent_change_24h']
+    percent_w = data[0]['percent_change_7d']
     dict_prices[currencie] = value
     dict_percent[currencie] = percent_h
     dict_percent_d[currencie] = percent_d
     dict_percent_w[currencie] = percent_w
-    value = float(dict_prices[currencie]) * float(dict_amount[currencie])
-    plus_value = value - (float(dict_amount[currencie]) * float(dict_buying_prices[currencie]))
+    try:
+        value = float(dict_prices[currencie]) * float(dict_amount[currencie])
+        plus_value = value - (float(dict_amount[currencie]) * float(dict_buying_prices[currencie]))
+    except ValueError:
+        value = 0
+        plus_value = 0
     color = colorT(plus_value)
     color_percent_h = colorT(percent_h)
     color_percent_d = colorT(percent_d)
     color_percent_w = colorT(percent_w)
     final_list.append([currencie,
-        dict_prices[currencie],
-        colored(dict_percent[currencie], color_percent_h),
-        colored(dict_percent_d[currencie], color_percent_d),
-        colored(dict_percent_w[currencie], color_percent_w),
-        dict_amount[currencie],
-        round(value,1),
-        colored(round(plus_value,1), color)])
+                      dict_prices[currencie],
+                      colored(dict_percent[currencie], color_percent_h),
+                      colored(dict_percent_d[currencie], color_percent_d),
+                      colored(dict_percent_w[currencie], color_percent_w),
+                      dict_amount[currencie],
+                      round(value,1),
+                      colored(round(plus_value,1), color)])
     total += value
     total_earned += plus_value
 color = colorT(total_earned)
-final_list.append([""])
-final_list.append(["TOTAL", colored(str(int(total)), "green")])
-final_list.append(["EARNED", colored(str(int(total_earned)), color)])
-print(tabulate(final_list, headers=headers, tablefmt="simple", numalign="right"))
+final_list.append([''])
+final_list.append(['TOTAL', colored(str(int(total)), 'green')])
+final_list.append(['EARNED', colored(str(int(total_earned)), color)])
+print tabulate(final_list, headers=headers, tablefmt="simple", numalign="right")
